0f136642fb48e4d2abf4e42767c800518ccb0fb3
brwitten <bethrwitten@gmail.com>
2017-12-05 13:57:54 -0800

    extracting readable data through ruby-readability

--- app/controllers/magazine_controller.rb
+++ app/controllers/magazine_controller.rb
@@ -1,2 +1,22 @@
+require 'readability'
+require 'open-uri'
+
+
+# doc = Nokogiri::HTML(open("https://www.nytimes.com/2017/12/05/us/politics/supreme-court-same-sex-marriage-cake.html"))
+# @parsed = doc.css("p[class='story-body-text story-content']").text
+# puts @parsed
+
+
+source = open('https://www.nytimes.com/2017/12/05/us/politics/supreme-court-same-sex-marriage-cake.html').read
+puts Readability::Document.new(source).content
+
+
+
+# @parsed.each do |text|
+#   puts text
+# end
+
+
 class MagazineController < ApplicationController
+
 end
