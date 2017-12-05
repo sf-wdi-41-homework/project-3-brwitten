f4cd749877e1a4790b10fa2a23a5c719790527ce
brwitten <bethrwitten@gmail.com>
2017-12-05 14:38:58 -0800

    diffbot api is working to parse article

--- app/controllers/magazine_controller.rb
+++ app/controllers/magazine_controller.rb
@@ -1,22 +1,15 @@
-require 'readability'
-require 'open-uri'
-
-
-# doc = Nokogiri::HTML(open("https://www.nytimes.com/2017/12/05/us/politics/supreme-court-same-sex-marriage-cake.html"))
-# @parsed = doc.css("p[class='story-body-text story-content']").text
-# puts @parsed
-
-
-source = open('https://www.nytimes.com/2017/12/05/us/politics/supreme-court-same-sex-marriage-cake.html').read
-puts Readability::Document.new(source).content
-
-
-
-# @parsed.each do |text|
-#   puts text
-# end
-
+require 'httparty'
 
 class MagazineController < ApplicationController
+
+  def parse_article
+     token = "bb383b3d967eeea80833c684c7d67164"
+     url = "https://www.nytimes.com/2017/12/05/us/politics/supreme-court-same-sex-marriage-cake.html"
+     response = HTTParty.get "https://api.diffbot.com/v3/analyze?token=#{token}&url=#{url}"
+     @response = response
+     puts "THIS IS YOUR RESPONSE"
+     puts response
+     render('index')
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/twonouns", to: "twonouns#index", as: "twonouns"
   get "/magazine", to: "magazine#index", as: "magazine"
+  get "/parse_article" => "magazine#parse_article"
 
 
 end
