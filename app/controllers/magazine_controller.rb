f4b55747032e897b3f5d0b57ce4d67c20d3770b4
brwitten <bethrwitten@gmail.com>
2017-12-05 16:08:25 -0800

    parsed JSON is rendering on page

--- app/controllers/magazine_controller.rb
+++ app/controllers/magazine_controller.rb
@@ -7,8 +7,13 @@ class MagazineController < ApplicationController
      url = params[:search]
      response = HTTParty.get "https://api.diffbot.com/v3/analyze?token=#{token}&url=#{url}"
      @response = response
-     puts "THIS IS YOUR RESPONSE"
-     puts response
+     @title = response["objects"][0]["title"]
+     @author = response["objects"][0]["author"]
+     @source = response["objects"][0]["siteName"]
+     @url = response["request"]["pageUrl"]
+     @date = response["objects"][0]["date"]
+     @text = response["objects"][0]["text"]
+
      render('user/index.html.erb')
   end
 
